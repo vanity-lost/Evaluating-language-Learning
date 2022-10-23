b514b0beab7ac12b66d45ed4d25d239f6da9053d
Randolph <randolph.zhao.49@gmail.com>
2022-10-23 15:45:09 -0500

    Update Embedding Model

--- parameters.py
+++ parameters.py
@@ -1,13 +1,17 @@
-TRAIN_DIR = 'train.csv'
+TRAIN_DIR = {
+    'current': './dataset/current/train.csv',
+    'old': './dataset/old/train.csv',
+    'hewlett': './dataset/hewlett/train.csv'
+}
 TEST_DIR = 'test.csv'
 SUBMISSION_DIR = 'submission.csv'
 
+# Embedding model
 DEBERTA = 'microsoft/deberta-v3-base'
 DEBERTA_DIR = './deberta'
 
-
+# Embedding training
 MAX_LENGTH = 512
 BATCH_SIZE = 4
-
 TRAIN_EMBEDDING = 'train_embeddings.npy'
 TEST_EMBEDDING = 'test_embeddings.npy'
